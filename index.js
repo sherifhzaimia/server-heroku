bac7af34711620a97a4d536f3d6bd81e2ec2f5bd
sherif <hzaimiacherif@gmail.com>
2024-11-01 01:59:28 +0100

    Update server to extract session token

--- index.js
+++ index.js
@@ -15,7 +15,14 @@ async function extractSessionToken(res) {
   try {
     const browser = await puppeteer.launch({
       headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-gpu",
+        "--no-zygote",
+        "--single-process",
+      ]
     });
 
     const page = await browser.newPage();
